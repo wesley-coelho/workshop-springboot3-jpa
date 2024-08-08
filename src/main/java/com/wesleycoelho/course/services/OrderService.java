ba128c6dabd7d7d3c1a2a0ca5cd9236be3f61e85
Wesley Coelho <wesley_coelho2003@yahoo.com.br>
2024-08-08 16:31:24 -0300

    Order, Instant, ISO 8601 - PART 2

---
+++ src/main/java/com/wesleycoelho/course/services/OrderService.java
@@ -0,0 +1,29 @@
+package com.wesleycoelho.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wesleycoelho.course.entities.Order;
+import com.wesleycoelho.course.entities.User;
+import com.wesleycoelho.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+		
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
