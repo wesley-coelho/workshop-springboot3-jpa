60647b4f2f2feaa1f2b57032d4deb2c969c3cdab
Wesley Coelho <wesley_coelho2003@yahoo.com.br>
2024-08-12 18:17:03 -0300

    Payment one-to-one association

--- src/main/java/com/wesleycoelho/course/config/TestConfig.java
+++ src/main/java/com/wesleycoelho/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.wesleycoelho.course.entities.Category;
 import com.wesleycoelho.course.entities.Order;
 import com.wesleycoelho.course.entities.OrderItem;
+import com.wesleycoelho.course.entities.Payment;
 import com.wesleycoelho.course.entities.Product;
 import com.wesleycoelho.course.entities.User;
 import com.wesleycoelho.course.entities.enums.OrderStatus;
@@ -85,6 +86,9 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 		
 	}
 	
