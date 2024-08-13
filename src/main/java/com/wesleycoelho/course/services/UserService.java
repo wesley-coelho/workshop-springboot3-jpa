6c3e73d8401ca846539da56260a6b51bc95c556d
Wesley Coelho <wesley_coelho2003@yahoo.com.br>
2024-08-13 14:27:11 -0300

    Exception handling - update

--- src/main/java/com/wesleycoelho/course/services/UserService.java
+++ src/main/java/com/wesleycoelho/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.wesleycoelho.course.repositories.UserRepository;
 import com.wesleycoelho.course.services.exceptions.DatabaseException;
 import com.wesleycoelho.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -45,10 +47,15 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
+		
 
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
